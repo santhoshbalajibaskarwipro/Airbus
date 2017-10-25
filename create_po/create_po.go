9cf26fb0d8db0d79d895bc9eb3ae592d662931c3
santhoshbalajibaskarwipro <32066907+santhoshbalajibaskarwipro@users.noreply.github.com>
2017-10-25 19:27:12 +0530

    Update create_po.go

--- create_po/create_po.go
+++ create_po/create_po.go
@@ -37,7 +37,7 @@ type po_order struct{
 	sap_po_order string `json:"sap_po_order"`	
 	supplier string `json:"supplier"`
 	venderso string `json:"venderso"`
-	
+	wipro_order_reference string `json:"wipro_order_reference"`
 	
 }
 
@@ -139,7 +139,7 @@ func (t *Manage_po_order) Query(stub shim.ChaincodeStubInterface, function strin
 
 func (t *Manage_po_order) create_po_order_id(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var err error
-	if len(args) != 3 {
+	if len(args) != 4 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 9")
 	}
 	fmt.Println("Creating a new Form for po order id ")
@@ -152,18 +152,21 @@ func (t *Manage_po_order) create_po_order_id(stub shim.ChaincodeStubInterface, a
 	if len(args[2]) <= 0 {
 		return nil, errors.New("3rd argument must be a non-empty string")
 	}
-	
+	if len(args[3]) <= 0 {
+		return nil, errors.New("4th argument must be a non-empty string")
+	}
 	
 	sap_po_order := args[0]
 	supplier := args[1]
 	venderso := args[2]
-		
+	wipro_order_reference := arg[3]	
 	
 		
 	//build the Form json string manually
 	input := 	`{`+
 		`"sap_po_order": "` + sap_po_order + `" , `+
 		`"supplier": "` + supplier + `" , `+ 
+		`"wipro_order_reference": "` + wipro_order_reference + `" , `+ 
 		`"venderso": "` + venderso + `"`+
 		`}`
 		fmt.Println("input: " + input)
